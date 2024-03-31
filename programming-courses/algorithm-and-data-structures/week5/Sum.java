5c832b3db1a29002d433365320b9866622403a10
Kevv-thon <kevinbramastaa@gmail.com>
2024-04-01 02:29:53 +0700

    feat: multiple company can now be calculate

--- programming-courses/algorithm-and-data-structures/week5/Sum.java
+++ programming-courses/algorithm-and-data-structures/week5/Sum.java
@@ -35,26 +35,42 @@ public class Sum {
     static Scanner inp = new Scanner(System.in);
 
     public static void main(String[] args) {
-        
-        
-        System.out.println("=====================================================");
-        System.out.println("Program for calculating total profits");
-        System.out.print("Input the number of month : ");
-        int totElemn = inp.nextInt();
-        Sum sm = new Sum(totElemn);
 
         System.out.println("=====================================================");
-        for (int i = 0; i < sm.elemen; i++) {
-            System.out.println("Input the profit of the month to - " + (i + 1) + " = ");
-            sm.profit[i] = inp.nextDouble();
+        System.out.println("Program for calculating total profits");
+        System.out.print("Input how many company you want to calculate: ");
+        int companyCount = inp.nextInt();
+        Sum[] sm = new Sum[companyCount];
+
+        System.out.println("=====================================================");
+        System.out.print("Input the number of month : ");
+        int totElemn = inp.nextInt();
+
+        for (int i = 0; i < companyCount; i++) {
+            sm[i] = new Sum(totElemn);
+        }
+
+        System.out.println("=====================================================");
+        for (int i = 0; i < sm.length; i++) {
+            System.out.println("Company " + (i + 1));
+            for (int j = 0; j < sm[0].elemen; j++) {
+                System.out.println("Input the profit of the month to - " + (i + 1) + " = ");
+                sm[i].profit[j] = inp.nextDouble();
+            }
         }
 
         System.out.println("=====================================================");
         System.out.println("Algorithm brute force");
-        System.out.println("Total profits of the company for " + sm.elemen + " month is = " + sm.totalBF(sm.profit));
+        for (int i = 0; i < sm.length; i++) {
+            System.out.println("Total profits of company " + (i + 1) + " for " + sm[i].elemen + " month is = "
+                    + sm[i].totalBF(sm[i].profit));
+        }
+
         System.out.println("=====================================================");
         System.out.println("Algorithm divide and conquer");
-        System.out.println("Total profits of the company for " + sm.elemen + " month is = "
-                + sm.totalDC(sm.profit, 0, sm.elemen - 1));
+        for (int i = 0; i < sm.length; i++) {
+            System.out.println("Total profits of company " + (i + 1) + " for " + sm[i].elemen + " month is = "
+                    + sm[i].totalDC(sm[i].profit, 0, sm[i].elemen - 1));
+        }
     }
 }
