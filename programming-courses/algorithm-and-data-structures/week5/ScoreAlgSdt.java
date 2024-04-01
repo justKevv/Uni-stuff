c1f66ec9ee603266ede00666b39d5577204c80ab
Kevv-thon <kevinbramastaa@gmail.com>
2024-04-01 07:28:37 +0700

    feat: Added Divided And Conquer

--- programming-courses/algorithm-and-data-structures/week5/ScoreAlgSdt.java
+++ programming-courses/algorithm-and-data-structures/week5/ScoreAlgSdt.java
@@ -10,7 +10,7 @@ public class ScoreAlgSdt {
         return (ass * 0.3) + (quiz * 0.2) + (mid * 0.2) + (finals * 0.3);
     }
 
-    static double averageScore(ScoreAlgSdt[] students) {
+    static double averageScoreBF(ScoreAlgSdt[] students) {
         double totalScore = 0;
         for (ScoreAlgSdt student : students) {
             totalScore += student.CalculateTotalScore(student.scoreAssgnments, student.scoreQuiz, student.scoreMid,
@@ -18,6 +18,19 @@ public class ScoreAlgSdt {
         }
         return totalScore / students.length;
 
+    }
+
+    static double averageScoreDC(ScoreAlgSdt[] students, int start, int end) {
+        if (start == end) {
+            return students[start].CalculateTotalScore(students[start].scoreAssgnments, students[start].scoreQuiz,
+                students[start].scoreMid, students[start].scoreFinal);
+        } else {
+            int mid = (start + end) / 2;
+            double leftScore = averageScoreDC(students, start, mid);
+            double rightScore = averageScoreDC(students, mid + 1, end);
+            int totalStudents = (end - start) + 1;
+            return ((leftScore * (mid - start + 1)) + (rightScore * (end - mid))) / totalStudents;
+        }
     }
 
     static Scanner sc = new Scanner(System.in);
@@ -47,6 +60,7 @@ public class ScoreAlgSdt {
                             students[i].scoreMid, students[i].scoreFinal));
         }
 
-        System.out.printf("%-10s | %10s%n ", "Average", averageScore(students));
+        System.out.printf("%-10s | %10s%n ", "Average BF", averageScoreBF(students));
+        System.out.printf("%-10s | %10s%n ", "Average DC", averageScoreDC(students, 0, students.length - 1));
     }
 }
