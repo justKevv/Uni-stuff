3e05279bdd83499fe5b77aac0aebde593957f11d
Kevv-thon <kevinbramastaa@gmail.com>
2024-04-05 14:23:45 +0700

    feat: Added Assingment 3

---
+++ programming-courses/algorithm-and-data-structures/week7/src/main/java/org/example/Array.java
@@ -0,0 +1,82 @@
+package org.example;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Array {
+    int[] data = new int[5];
+    int idx = 0;
+    public void add(int number) {
+        if (idx < data.length) {
+            data[idx] = number;
+            idx++;
+        }
+    }
+
+    void selectionSort() {
+        for (int i = 0; i < data.length - 1; i++) {
+            int smallest = i;
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[j] < data[smallest]) {
+                    smallest = j;
+                }
+            }
+            int temp = data[smallest];
+            data[smallest] = data[i];
+            data[i] = temp;
+        }
+    }
+
+    public void display() {
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
+    }
+
+    int seqSearch(int search) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == search) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    void showPosition(int x, int pos) {
+        if (pos == -1) {
+            System.out.println("Data: " + x + " is not found");
+        } else {
+            System.out.println("Data: " + x + " is found at position " + pos);
+        }
+    }
+
+    void Biggest() {
+        System.out.println("Biggest number: " + data[data.length - 1]);
+    }
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Array data = new Array();
+        Random rand = new Random();
+        
+
+        System.out.println("Input how many numbers you want: ");
+        int amount = scanner.nextInt();
+        data.data = new int[amount];
+
+        for (int i = 0; i < amount; i++) {
+            data.add(rand.nextInt(100));
+        }
+
+        data.display();
+        data.selectionSort();
+        data.display();
+        int search = data.data[rand.nextInt(data.data.length)];
+        System.out.println("Search "+ search);
+        int position = data.seqSearch(search);
+        data.showPosition(search, position);
+        data.Biggest();
+    }
+}
