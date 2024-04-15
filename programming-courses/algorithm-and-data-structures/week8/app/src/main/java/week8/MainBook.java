9956b4853f75ae83f62c0b78786bbfb03355e62d
Kevv-thon <kevinbramastaa@gmail.com>
2024-04-16 02:32:53 +0700

    feat: User can now choose the command via terminal

--- programming-courses/algorithm-and-data-structures/week8/app/src/main/java/week8/MainBook.java
+++ programming-courses/algorithm-and-data-structures/week8/app/src/main/java/week8/MainBook.java
@@ -10,23 +10,44 @@ public class MainBook {
         StackBook st = new StackBook(8);
         Faker faker = new Faker();
         Random r = new Random();
-        
 
         char choose = 't';
-        int numbers = 0;
-        do {
-            Book bk = new Book(faker.book().title(), faker.book().author(), (1900 + r.nextInt(125)), r.nextInt(500),
-                    r.nextInt(5000));
-            System.out.println("Added random book " + (numbers + 1));
-            numbers++;
-            System.out.println("Do you want to add another book?");
-            choose = sc.next().charAt(0);
-            st.push(bk);
-        } while (choose == 'y');
+        
+        int stack = 0;
 
-        st.print();
-        st.pop();
-        st.peek();
-        st.print();
+        do {
+            System.out.println("What do you want to do?");
+            System.out.println("1. Push");
+            System.out.println("2. Pop");
+            System.out.println("3. Peek");
+            System.out.println("4. Print");
+            System.out.println("5. Exit");
+            stack = sc.nextInt();
+
+            switch (stack) {
+                case 1:
+                    do {
+                        int numbers = st.top;
+                        Book bk = new Book(faker.book().title(), faker.book().author(), (1900 + r.nextInt(125)),
+                                r.nextInt(500),
+                                r.nextInt(5000));
+                        System.out.println("Added random book " + (numbers + 1));
+                        numbers++;
+                        System.out.println("Do you want to add another book?");
+                        choose = sc.next().charAt(0);
+                        st.push(bk);
+                    } while (choose == 'y');
+                    break;
+                case 2:
+                    st.pop();
+                    break;
+                case 3:
+                    st.peek();
+                    break;
+                case 4:
+                    st.print();
+                    break;
+            }
+        } while (stack != 5);
     }
 }
