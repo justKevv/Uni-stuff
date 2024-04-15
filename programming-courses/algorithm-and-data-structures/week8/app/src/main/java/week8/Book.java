d1157f61dcf62885791184fe97214356f9864fc5
Kevv-thon <kevinbramastaa@gmail.com>
2024-04-16 02:15:21 +0700

    feat: Added Practicumm 2

---
+++ programming-courses/algorithm-and-data-structures/week8/app/src/main/java/week8/Book.java
@@ -0,0 +1,15 @@
+package week8;
+
+public class Book {
+    String title, authorName;
+    int publishedYear, pageAmount, price;
+
+    Book(String title, String authorName, int publishedYear, int pageAmount, int price) {
+        this.title = title;
+        this.authorName = authorName;
+        this.publishedYear = publishedYear;
+        this.pageAmount = pageAmount;
+        this.price = price;
+        
+    }
+}
---
+++ programming-courses/algorithm-and-data-structures/week8/app/src/main/java/week8/MainBook.java
@@ -0,0 +1,31 @@
+package week8;
+
+import net.datafaker.Faker;
+import java.util.Scanner;
+import java.util.Random;
+
+public class MainBook {
+    public static void main(String[] args) {
+        StackBook st = new StackBook(8);
+        Faker faker = new Faker();
+        Random r = new Random();
+        Scanner sc = new Scanner(System.in);
+
+        char choose = 't';
+        int numbers = 0;
+        do {
+            Book bk = new Book(faker.book().title(), faker.book().author(), (1900 + r.nextInt(125)), r.nextInt(500),
+                    r.nextInt(5000));
+            System.out.println("Added random book " + (numbers + 1));
+            numbers++;
+            System.out.println("Do you want to add another book?");
+            choose = sc.next().charAt(0);
+            st.push(bk);
+        } while (choose == 'y');
+
+        st.print();
+        st.pop();
+        st.peek();
+        st.print();
+    }
+}
