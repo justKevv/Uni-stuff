4ba690a48d0cc8eee518c517a6c1b1d1dee577e3
Kevv-thon <kevinbramastaa@gmail.com>
2024-06-04 03:21:46 +0700

    feat: Added Menu to TreeMain

--- programming-courses/algorithm-and-data-structures/week14/app/src/main/java/week14/BinaryTreeMain.java
+++ programming-courses/algorithm-and-data-structures/week14/app/src/main/java/week14/BinaryTreeMain.java
@@ -1,29 +1,56 @@
 package week14;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class BinaryTreeMain {
+
+    static void menu() {
+        System.out.println("");
+        System.out.println("1. Add");
+        System.out.println("2. Delete");
+        System.out.println("3. Find");
+        System.out.println("4. Traverse In-Order");
+        System.out.println("5. Traverse Pre-Order");
+        System.out.println("6. Traverse Post-Order");
+        System.out.println("7. Exit");
+    } 
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
+        Random rand = new Random();
 
-        tree.add(5);
-        tree.add(3);
-        tree.add(7);
-        tree.add(2);
-        tree.add(4);
-        tree.add(6);
-        tree.add(8);
-        tree.add(1);
-        tree.add(9);
-        tree.add(10);
-
-        tree.traversePreOrder(tree.root);
-        System.out.println("");
-        tree.traverseInOrder(tree.root);
-        System.out.println("");
-        tree.traversePostOrder(tree.root);
-        System.out.println("");
-        System.out.println("Find "+tree.find(8));
-        tree.delete(1);
-        tree.traversePreOrder(tree.root);
-        System.out.println("");
+        int choose;
+        do {
+            menu();
+            System.out.print("Choose: ");
+            choose = sc.nextInt();
+            switch (choose) {
+                case 1:
+                    tree.add(rand.nextInt(100));
+                    break;
+                case 2:
+                    System.out.print("Enter data to be deleted: ");
+                    tree.delete(sc.nextInt());
+                    break;
+                case 3:
+                    System.out.print("Enter data to be found: ");
+                    System.out.println(tree.find(sc.nextInt()));
+                    break;
+                case 4:
+                    tree.traverseInOrder(tree.root);
+                    break;
+                case 5:
+                    tree.traversePreOrder(tree.root);
+                    break;
+                case 6:
+                    tree.traversePostOrder(tree.root);
+                    break;
+                case 7:
+                    break;
+                default:
+                    System.out.println("Invalid input");
+            }
+        } while (choose != 7);
     }
 }
